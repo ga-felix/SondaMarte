f5315668ed1b90653d825a4df2dfb47270e2b7d4
Gabriel Felix de Souza Lopes <44716827+ga-felix@users.noreply.github.com>
2021-11-16 12:07:31 -0300

    Adiciona movimento da Sonda
    A classe Sonda implementa método de movimentação.

--- Marte/src/Sonda.java
+++ Marte/src/Sonda.java
@@ -4,21 +4,33 @@ import java.util.Map;
 class Sonda implements ObjetoMovel {
 
     private int[] posicao;
+    private String pontoCardeal;
     private Map<String, int[]> direcao;
 
-    Sonda(int x, int y, String direcao) {
+    Sonda(int x, int y, String pontoCardeal) {
         this.posicao = new int[]{x, y};
-        this.direcao = new HashMap<String, int[]>() {
-            {
-                put("N", new int[]{0, 1});
-                put("S", new int[]{0, -1});
-                put("E", new int[]{1, 0});
-                put("W", new int[]{-1, 0});
-            }
-        };
+        this.pontoCardeal = pontoCardeal;
+        this.direcao = new HashMap<String, int[]>();
+        this.direcao.put("N", new int[]{0, 1});
+        this.direcao.put("S", new int[]{0, -1});
+        this.direcao.put("E", new int[]{1, 0});
+        this.direcao.put("W", new int[]{-1, 0});
+    }
+
+    private int[] somarVetores2D(int[] vetorA, int[] vetorB) {
+        return new int[]{vetorA[0] + vetorB[0], vetorA[1] + vetorB[1]};
+    }
+
+    public int[] posicaoAtual() {
+        return this.posicao;
+    }
+
+    public String apontaPara() {
+        return this.pontoCardeal;
     }
 
     public void mover() {
-
+        int[] direcao = this.direcao.get(this.apontaPara());
+        this.posicao = this.somarVetores2D(this.posicao, direcao);
     }
 }
