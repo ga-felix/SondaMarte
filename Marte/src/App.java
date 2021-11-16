0f23b4a8bae397654fcba1afb4c2cb824e07a092
Gabriel Felix de Souza Lopes <44716827+ga-felix@users.noreply.github.com>
2021-11-16 13:35:34 -0300

    Adiciona testes no método main

--- Marte/src/App.java
+++ Marte/src/App.java
@@ -1,7 +1,17 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá, mundo!");
-        Sonda sondaTeste = new sondaTeste(0, 0, "N");
-        
+        Sonda sondaTeste = new Sonda(0, 0, "N");
+        System.out.println("Posição atual: " + Arrays.toString(sondaTeste.posicaoAtual()));
+        sondaTeste.lerSinal('M');
+        System.out.println("Nova posição: " + Arrays.toString(sondaTeste.posicaoAtual()));
+        sondaTeste.lerSinal('L');
+        System.out.println("Nova direção cardinal: " + sondaTeste.apontaPara());
+        sondaTeste.lerSinal('L');
+        System.out.println("Nova direção cardinal: " + sondaTeste.apontaPara());
+        sondaTeste.lerSinal('M');
+        System.out.println("Nova posição: " + Arrays.toString(sondaTeste.posicaoAtual()));
     }
 }
